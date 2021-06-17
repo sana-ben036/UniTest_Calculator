dce579c1b0cf3f802f1e62a8322174a69b11f21e
SANA BEN <sana.bengannoune@gmail.com>
2021-06-17 16:43:21 +0100

    UI done

---
+++ MyApp/MyApp.UnitTest/MathUnitTest1.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyApp.ViewModels;
+
+namespace MyApp.UnitTest
+{
+    [TestClass]
+    public class MathUnitTest1
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            // Arrange
+
+            var vm = new MathViewModel();
+            vm.ValueA = 4;
+            vm.ValueB = 6;
+
+            //Act
+
+            vm.AddCommand.Execute(null);
+
+            //Assert
+
+            Assert.IsTrue(vm.Result == 9, "vm.Result != 10 !");
+
+        }
+    }
+}
---
+++ MyApp/MyApp/MyApp/ViewModels/MathViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MyApp.ViewModels
+{
+    public class MathViewModel : INotifyPropertyChanged
+    {
+        private int _valueA;
+        private int _valueB;
+        private int _result;
+
+        public int ValueA
+        {
+            get { return _valueA; }
+            set
+            {
+                _valueA = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int ValueB
+        {
+            get { return _valueB; }
+            set
+            {
+                _valueB = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    Result = _valueA + _valueB;
+                });
+            }
+
+        }
+
+        public ICommand MultiplyCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    Result = _valueA * _valueB;
+                });
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
